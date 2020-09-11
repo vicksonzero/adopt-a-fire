38cbe348f489cb92922dee9d971a84fe4dc4e5c4
Chui Hin Wah (Dickson) <dicksonZero@gmail.com>
2020-09-11 17:09:42 +0800

    Add tree, remove random chop wood

--- src/app/drag.js
+++ src/app/drag.js
@@ -1,5 +1,9 @@
 // adapted from https://github.com/xem/miniDragAndDrop
 
+
+
+const dist = exports.dist = (dx, dy) => Math.sqrt(dx * dx + dy * dy);
+
 // D = drag and drop persistent thing
 exports.dragAndDrop = delegates => {
     // declare all variables at the front to help minify
@@ -18,6 +22,12 @@ exports.dragAndDrop = delegates => {
     /** @type number */
     let dragY;
     /** @type number */
+    let startX;
+    /** @type number */
+    let startY;
+    /** @type number */
+    let maxDist;
+    /** @type number */
     let dragOffsetX;
     /** @type number */
     let dragOffsetY;
@@ -31,6 +41,12 @@ exports.dragAndDrop = delegates => {
     const pointerdown = e => {
         e.preventDefault();
         isDragging = 1;
+        if (dragContainer) {
+            dragContainer.remove();
+            dragContainer = null;
+            // isMove
+            dragOrigin.style.visibility = "visible";
+        }
         dragOrigin = null;
         dragContainer = null;
         if (e.touches) {
@@ -41,20 +57,23 @@ exports.dragAndDrop = delegates => {
         } else {
             dragOrigin = e.target;
         }
+
         while (dragOrigin != d && !dragOrigin.classList.contains("drag")) {
             dragOrigin = dragOrigin.parentNode;
         }
+
         if (dragOrigin == d) {
             dragOrigin = null;
         } else if (dragOrigin.parentNode.classList.contains("move") || dragOrigin.parentNode.classList.contains("copy")) {
             onDrag();
-            dragX = e.touches ? e.touches[0].pageX : e.pageX;
-            dragY = e.touches ? e.touches[0].pageY : e.pageY;
+            startX = dragX = e.touches ? e.touches[0].pageX : e.pageX;
+            startY = dragY = e.touches ? e.touches[0].pageY : e.pageY;
+            maxDist = 0;
             const globalRect = dragOrigin.getBoundingClientRect();
             dragOffsetX = dragX - globalRect.left;
             dragOffsetY = dragY - globalRect.top;
             dragContainer = d.createElement('div');
-            dragContainer.appendChild(dragOrigin.cloneNode(true))
+            dragContainer.appendChild(dragOrigin.cloneNode(true));
             d.body.appendChild(dragContainer);
             if (dragOrigin.parentNode.classList.contains("move")) {
                 dragOrigin.style.visibility = "hidden";
@@ -65,8 +84,8 @@ exports.dragAndDrop = delegates => {
             s.pointerEvents = "none";
             s.width = dragOrigin.clientWidth + 'px';
             s.height = dragOrigin.clientHeight + 'px';
-            s.left = dragX - dragOffsetX - 8 + "px";
-            s.top = dragY - dragOffsetY - 8 + "px";
+            s.left = dragX - dragOffsetX + "px";
+            s.top = dragY - dragOffsetY + "px";
         }
     }
     const pointermove = e => {
@@ -74,14 +93,19 @@ exports.dragAndDrop = delegates => {
         if (isDragging && dragContainer) {
             dragX = (e.touches ? e.touches[0].pageX : e.pageX);
             dragY = (e.touches ? e.touches[0].pageY : e.pageY);
-            dragContainer.style.left = dragX - dragOffsetX - 8 + "px";
-            dragContainer.style.top = dragY - dragOffsetY - 8 + "px";
+            const dd = dist(dragX - startX, dragY - startY);
+            // console.log('dd', dd);
+            maxDist = Math.max(dd, maxDist);
+            if (maxDist > 10) {
+                dragContainer.style.left = dragX - dragOffsetX - 8 + "px";
+                dragContainer.style.top = dragY - dragOffsetY - 8 + "px";
+            }
         }
     }
 
     const pointerup = e => {
-        e.preventDefault();
         if (dragContainer) {
+            e.preventDefault();
             isDragging = 0;
             if (e.touches) {
                 dropTarget = d.elementFromPoint(
@@ -98,7 +122,7 @@ exports.dragAndDrop = delegates => {
             }
             if (dropTarget != d) { // if have a dropTarget
                 let a;
-                doDropResponse = (a = onDrop(dragContainer.firstChild, dropTarget), a == null || !!a);
+                doDropResponse = (a = onDrop(e, maxDist, dragContainer.firstChild, dragOrigin, dropTarget), a == null || !!a);
                 if (doDropResponse) {
                     newElement = dropTarget.appendChild(dragContainer.firstChild.cloneNode(true));
                     newElement.style.position = "";
@@ -111,6 +135,7 @@ exports.dragAndDrop = delegates => {
                 }
             }
             dragContainer.remove();
+            dragContainer = null;
             // isMove
             dragOrigin.style.visibility = "visible";
         }
@@ -119,6 +144,7 @@ exports.dragAndDrop = delegates => {
     addEventListener("touchstart", pointerdown, { passive: false });
     addEventListener("mousemove", pointermove);
     addEventListener("touchmove", pointermove, { passive: false });
-    addEventListener("mouseup", pointerup);
-    addEventListener("touchend", pointerup, { passive: false });
+    window.addEventListener("mouseup", pointerup);
+    window.addEventListener("touchend", pointerup, { passive: false });
+    window.addEventListener("mouseout", pointerup);
 };
